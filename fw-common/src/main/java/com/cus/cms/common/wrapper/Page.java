86efa3d21b67ed8b384d7fcb9eef5d9d47371eb2
beyondfengyu <594981878@qq.com>
2017-02-14 00:47:26 +0800

    init

---
+++ fw-common/src/main/java/com/cus/cms/common/wrapper/Page.java
@@ -0,0 +1,45 @@
+package com.cus.cms.common.wrapper;
+
+import java.util.List;
+
+/**
+ * @author Andy
+ */
+public class Page<T> {
+    private long total;
+    private int pageSize;
+    private int pageNumber;
+    private List<T> rows;
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public List<T> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<T> rows) {
+        this.rows = rows;
+    }
+}
