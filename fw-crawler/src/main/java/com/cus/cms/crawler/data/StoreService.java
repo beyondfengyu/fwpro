35c8996c1992d5509bd5f0dd2b1002edc04da56f
beyondfengyu <594981878@qq.com>
2017-02-17 02:10:11 +0800

    init data

--- fw-crawler/src/main/java/com/cus/cms/crawler/data/StoreService.java
+++ fw-crawler/src/main/java/com/cus/cms/crawler/data/StoreService.java
@@ -23,27 +23,27 @@ public class StoreService {
     }
 
     public void saveUrl(List<String> linkUrls, int siteType){
-//        if(linkUrls == null || linkUrls.size() < 1){
-//            return;
-//        }
-//        try {
-//            List<Document> docs = new ArrayList<>();
-//            Document doc = null;
-//            for (String linkUrl : linkUrls) {
-//                int linkType = 1;
-//                if (linkUrl.endsWith("htm") || linkUrl.endsWith("html") || linkUrl.endsWith("shtml")) {
-//                    linkType = 2;
-//                }
-//                doc = new Document("link_url", linkUrl)
-//                        .append("link_type", linkType)
-//                        .append("site_type", siteType)
-//                        .append("status", 1);
-//                docs.add(doc);
-//            }
-//            docCollection.insertMany(docs);
-//        } catch (Exception e) {
-//            logger.error("saveUrl error", e);
-//        }
+        if(linkUrls == null || linkUrls.size() < 1){
+            return;
+        }
+        try {
+            List<Document> docs = new ArrayList<>();
+            Document doc = null;
+            for (String linkUrl : linkUrls) {
+                int linkType = 1;
+                if (linkUrl.endsWith("htm") || linkUrl.endsWith("html") || linkUrl.endsWith("shtml")) {
+                    linkType = 2;
+                }
+                doc = new Document("link_url", linkUrl)
+                        .append("link_type", linkType)
+                        .append("site_type", siteType)
+                        .append("status", 1);
+                docs.add(doc);
+            }
+            docCollection.insertMany(docs);
+        } catch (Exception e) {
+            logger.error("saveUrl error", e);
+        }
     }
 
     public void saveNav(String url, int dirType) {
@@ -53,45 +53,45 @@ public class StoreService {
         if (url.endsWith("/")) {
             url = url.substring(0, url.length() - 2);
         }
-//        try {
-//            MongoCollection<Document> dirCollection = MongodbHelper.getDbCollection("fwpro", "fw_dir");
-//            String[] arr = url.split("/");
-//            Document doc = new Document("dir_name", arr[arr.length - 1])
-//                    .append("dir_code", arr[arr.length - 1])
-//                    .append("dir_type", dirType)
-//                    .append("last_code", null)
-//                    .append("status", 1)
-//                    .append("show_order", 0);
-//            dirCollection.insertOne(doc);
-//        } catch (Exception e) {
-//            logger.error("saveNav error , url is " + url, e);
-//        }
+        try {
+            MongoCollection<Document> dirCollection = MongodbHelper.getDbCollection("fwpro", "fw_dir");
+            String[] arr = url.split("/");
+            Document doc = new Document("dir_name", arr[arr.length - 1])
+                    .append("dir_code", arr[arr.length - 1])
+                    .append("dir_type", dirType)
+                    .append("last_code", null)
+                    .append("status", 1)
+                    .append("show_order", 0);
+            dirCollection.insertOne(doc);
+        } catch (Exception e) {
+            logger.error("saveNav error , url is " + url, e);
+        }
     }
 
     public void batchSaveNav(List<String> urls, int dirType) {
-//        if(urls == null || urls.size() < 1){
-//            return;
-//        }
-//
-//        try {
-//            MongoCollection<Document> dirCollection = MongodbHelper.getDbCollection("fwpro", "fw_dir");
-//            List<Document> docs = new ArrayList<>();
-//            Document doc = null;
-//            for (String url : urls) {
-//                String[] arr = url.split("/");
-//                doc = new Document("dir_name", arr[arr.length - 1])
-//                        .append("dir_code", arr[arr.length - 1])
-//                        .append("dir_type", dirType)
-//                        .append("status", 1)
-//                        .append("show_order", 0);
-//                if (dirType != 1) {
-//                    doc.append("last_code", arr[arr.length - 2]);
-//                }
-//                docs.add(doc);
-//            }
-//            dirCollection.insertMany(docs);
-//        } catch (Exception e) {
-//            logger.error("batchSaveNav error", e);
-//        }
+        if(urls == null || urls.size() < 1){
+            return;
+        }
+
+        try {
+            MongoCollection<Document> dirCollection = MongodbHelper.getDbCollection("fwpro", "fw_dir");
+            List<Document> docs = new ArrayList<>();
+            Document doc = null;
+            for (String url : urls) {
+                String[] arr = url.split("/");
+                doc = new Document("dir_name", arr[arr.length - 1])
+                        .append("dir_code", arr[arr.length - 1])
+                        .append("dir_type", dirType)
+                        .append("status", 1)
+                        .append("show_order", 0);
+                if (dirType != 1) {
+                    doc.append("last_code", arr[arr.length - 2]);
+                }
+                docs.add(doc);
+            }
+            dirCollection.insertMany(docs);
+        } catch (Exception e) {
+            logger.error("batchSaveNav error", e);
+        }
     }
 }
