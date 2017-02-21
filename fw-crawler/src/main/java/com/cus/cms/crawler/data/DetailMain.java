5b8b8e1c14f1ee1103eaad8f318333111f32933c
laochunyu <laochunyu@yy.com>
2017-02-21 14:06:47 +0800

    init parse detail

--- fw-crawler/src/main/java/com/cus/cms/crawler/data/DetailMain.java
+++ fw-crawler/src/main/java/com/cus/cms/crawler/data/DetailMain.java
@@ -6,6 +6,9 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 
+import javax.print.Doc;
+import java.lang.Exception;
+import java.lang.System;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -18,6 +21,7 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class DetailMain {
 
+    private static final int count = 8;
     private static Lock lock;
     private static MongoCollection<Document> collLink;
     private static MongoCollection<Document> collPage;
@@ -29,11 +33,15 @@ public class DetailMain {
         collPage = MongodbHelper.getDbCollection("fwpro", "fw_page");
         collError = MongodbHelper.getDbCollection("fwpro", "fw_error");
 
-        ExecutorService service = Executors.newFixedThreadPool(8);
-        String[] detail = DataService.getDetailPage("http://www.diyifanwen.com/fanwen/lunwenzhidao/151262245273184.htm");
-        if (detail != null) {
-            System.out.println("title is: " + detail[0]);
-            System.out.println("content is: " + detail[1]);
+        ExecutorService service = Executors.newFixedThreadPool(count);
+//        String[] detail = DataService.getDetailPage("http://www.diyifanwen.com/fanwen/lunwenzhidao/151262245273184.htm");
+//        if (detail != null) {
+//            System.out.println("title is: " + detail[0]);
+//            System.out.println("content is: " + detail[1]);
+//        }
+
+        for (int i = 0; i < count; i++) {
+            service.execute(new Task());
         }
     }
 
@@ -43,17 +51,17 @@ public class DetailMain {
         public void run() {
             FindIterable<Document> iter = null;
             List<String> list = null;
-            while (true) {
+//            while (true) {
                 try {
                     final List<String> finalList = list = new ArrayList<>();
                     lock.lock();
-                    iter = collLink.find(new Document("status", 1)).limit(1);
+                    iter = collLink.find(new Document("status", 1)).sort(new Document("id",-1)).limit(10);
                     iter.forEach(new Block<Document>() {
                         public void apply(Document doc) {
                             String link = doc.getString("link_url");
                             finalList.add(link);
-                            doc.put("status", 2);
-                            collLink.updateOne(new Document("link_url", link), doc);
+                            Document newDoc = new Document("$set", new Document("status", 2));
+                            collLink.updateOne(new Document("link_url", link), newDoc);
                         }
                     });
                 } finally {
@@ -61,10 +69,25 @@ public class DetailMain {
                 }
 
                 for (String link : list) {
-
+                    try {
+                        String[] detail = DataService.getDetailPage(link);
+                        if (detail != null) {
+                            System.out.println("title is: " + detail[0]);
+                            Document newdoc = new Document();
+                            collPage.insertOne(newdoc);
+//                            System.out.println("content is: " + detail[1]);
+                        }else {
+                            Document doc = new Document("link_url", link).append("status", 1);
+                            collError.insertOne(doc);
+                        }
+                    } catch (Exception e) {
+                        System.out.println("page parse error, link is " + link);
+                        Document doc = new Document("link_url", link).append("status", 1);
+                        collError.insertOne(doc);
+                    }
                 }
 
-            }
+//            }
         }
     }
 }
